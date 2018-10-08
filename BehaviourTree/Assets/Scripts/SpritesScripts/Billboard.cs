c3adf317523feee8faaa7976856ea9180db780f4
Fiesta87 <jb@pommeret.fr>
2018-10-08 13:29:18 -0400

    init project, base setup and begining of animation work

---
+++ BehaviourTree/Assets/Scripts/SpritesScripts/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Billboard : MonoBehaviour {
+
+	void Awake () {
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		
+		sr.shadowCastingMode = ShadowCastingMode.On;
+		sr.receiveShadows = true;
+	}
+	void Start () {
+		this.transform.forward = Camera.main.transform.forward;
+	}
+}
