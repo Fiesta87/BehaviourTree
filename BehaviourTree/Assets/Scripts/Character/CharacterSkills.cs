2ce48bfc0f5a153b676c6031880a2139ffc62c82
Fiesta87 <jb@pommeret.fr>
2018-11-02 12:25:28 -0400

    characters skills

---
+++ BehaviourTree/Assets/Scripts/Character/CharacterSkills.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSkills : MonoBehaviour {
+
+	[Range(0, 10)]
+	public int negotiation;
+	[Range(0, 10)]
+	public int strength;
+}
